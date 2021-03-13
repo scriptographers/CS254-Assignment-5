92bd400bf40a8f67065828b55c43952adfed2df6
devansh-dvj <devansh.dvj@gmail.com>
2021-03-13 18:17:19 +0530

    Updated PostProcess for c0='1'

---
+++ Q1/CarryCell.vhd
@@ -0,0 +1,41 @@
+-- Component: Computes g and p for the pre-processing step
+library work;
+use work.all;
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity CarryCell is
+  port (
+    c0, G, P : in std_logic;
+    carry    : out std_logic
+  );
+end entity;
+
+architecture struct of CarryCell is
+
+  signal pcarry : std_logic;
+
+  component AndGate is
+    port (
+      a, b : in std_logic;
+      z    : out std_logic
+    );
+  end component;
+
+  component OrGate is
+    port (
+      a, b : in std_logic;
+      z    : out std_logic
+    );
+  end component;
+
+begin
+
+  prop_carry : AndGate
+  port map(a => P, b => c0, z => pcarry);
+
+  gen_carry : OrGate
+  port map(a => G, b => pcarry, z => carry);
+
+end architecture;
--- Q1/PostProcess.vhd
+++ Q1/PostProcess.vhd
@@ -7,9 +7,11 @@ use ieee.std_logic_1164.all;
 
 entity PostProcess is
 	port (
-		Gi0, p : in std_logic_vector (7 downto 0);
-		cout   : out std_logic;
-		sum    : out std_logic_vector (7 downto 0)
+		cin      : in std_logic;
+		Gi0, Pi0 : in std_logic_vector (7 downto 0);
+		p        : in std_logic_vector (7 downto 0);
+		cout     : out std_logic;
+		sum      : out std_logic_vector (7 downto 0)
 	);
 end entity;
 
@@ -24,20 +26,28 @@ architecture struct of PostProcess is
 		);
 	end component;
 
+	component CarryCell is
+		port (
+			c0, G, P : in std_logic;
+			carry    : out std_logic
+		);
+	end component;
+
 begin
 
-	-- Assuming c_0 = 0, thus c(i+1) = G[i:0]
-	c(0) <= '0';
-	init_carry : for I in 0 to 7 generate
-		c(I + 1) <= Gi0(I);
-	end generate init_carry;
-
-	cout <= c(8);
+	c(0) <= cin;
+	-- Parallel Carry computation using for...generate statement
+	carry_loop : for I in 1 to 8 generate
+		carry_i : CarryCell
+		port map(c0 => cin, G => Gi0(I - 1), P => Pi0(I - 1), carry => c(I));
+	end generate carry_loop;
 
 	-- Parallel Sum computation using for...generate statement
-	postprocess_loop : for I in 0 to 7 generate
+	sum_loop : for I in 0 to 7 generate
 		xor_i : XorGate
 		port map(a => p(I), b => c(I), z => sum(I));
-	end generate postprocess_loop;
+	end generate sum_loop;
+
+	cout <= c(8);
 
 end architecture;
